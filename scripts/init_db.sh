3ad5e09db01f4829d22e0b7c4523bb92e7d6fdba
Nick Rakochy <nrakochy@anduril.com>
2024-06-19 14:38:49 -0500

    Init db

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -33,12 +33,16 @@ POSTGRES_DB=${DB_NAME} \
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
 
-until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q';
+do
     >&2 echo "Postgres is still unavailable - sleeping"
-sleep 1 done
+    sleep 1
+done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 sqlx database create
+sqlx migrate run
+>&2 echo "Postgres has been migrated, ready to go!"
