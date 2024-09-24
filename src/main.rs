0d29549f0da60364dfbc0fa58e0cb210a11e686e
Nick Rakochy <nrakochy@anduril.com>
2024-09-24 13:41:30 -0500

    Bump sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
-
 use r2p::run;
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("http://127.0.0.1:8000").expect("Failed to bind it");
